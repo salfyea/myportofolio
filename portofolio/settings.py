3b2341c9e690163f71675893072a60b44d3fd796
salfyea <salwa.alifia@ui.ac.id>
2026-09-15 01:52:12 +0700

    Fix CSRF trusted origin for PWS

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -152,3 +152,8 @@ WHITENOISE_USE_FINDERS = True
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://salwa-alifia-myportofolio.pws.cs.ui.ac.id",
+]
+
