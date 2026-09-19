e71af26fc2875dfea96275b62a83659817135dbc
salfyea <salwa.alifia@ui.ac.id>
2026-09-20 02:22:35 +0700

    feat: register Skill in admin and seed existing skill data

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,8 @@
 
 from django.contrib import admin
 
-from .models import Experience, Project
+from .models import Experience, Project, Skill
 
 admin.site.register(Experience)
 admin.site.register(Project)
+admin.site.register(Skill)
